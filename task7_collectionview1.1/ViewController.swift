ef18b32ba75368c468185c73b2876dbd0d9fac90
KananMV <kenan2002memmedov@gmail.com>
2024-08-26 10:56:59 +0400

    Initial Commit

---
+++ task7_collectionview1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task7_collectionview1.1
+//
+//  Created by Kenan Memmedov on 26.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
